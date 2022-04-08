3918db30ac91a90f160386d0d89299134cb8cfb3
Moushu <1924246952@qq.com>
2022-04-08 18:34:12 +0800

    jujj

--- Stack/stack.go
+++ Stack/stack.go
@@ -4,3 +4,15 @@ type stackItem struct {
 	item interface{}
 	next *stackItem
 }
+
+type stack struct {
+	sp    *stackItem
+	depth uint64
+}
+
+// New new a stack
+func New() *stack {
+	var stack *stack = new(stack)
+	stack.depth = 0
+	return stack
+}
