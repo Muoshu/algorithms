57ba9e9373a121bd95d867f03aab784f14b9e9ed
Moushu <1924246952@qq.com>
2022-06-28 09:41:36 +0800

    g

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"algorithms/sort"
 	"fmt"
+	"time"
 )
 
 func compare(x, y interface{}) bool {
@@ -10,9 +10,21 @@ func compare(x, y interface{}) bool {
 }
 
 func main() {
-	var x = []int{53, 17, 78, 9, 45, 65, 87, 32}
-	fmt.Println("original:", x)
-	sort.HeapSort(x)
-	fmt.Println(x)
+	//var x = []int{53, 17, 78, 9, 45, 65, 87, 32}
+	//fmt.Println("original:", x)
+	//
+	//sort.HeapSort(x)
+	start := time.Now().Nanosecond()
+	fmt.Println(f(20))
+	end := time.Now().Nanosecond()
+	fmt.Println(end - start)
+
+}
+
+func f(n int) int {
+	if n == 0 || n == 1 {
+		return n
+	}
+	return f(n-1) + f(n-2)
 
 }
